64c0750dd4dcc0874e60b92ca7897924b9b64332
Marcel_Horvath <marcel.horvath@tul.cz>
2024-06-17 23:17:19 +0200

    grid hover

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -6,7 +6,6 @@ import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center w-full">
-
             <section
                 className="relative flex flex-col items-center justify-center h-[calc(100vh-4rem)] w-full">
                 <Grid/>
@@ -15,16 +14,19 @@ export default function Home() {
                         <p className="text-2xl"> Hi there!</p>
                         <h1 className="text-6xl font-bold mt-2">I&apos;m Marcel Horváth</h1>
                         <p className="text-lg mt-4 font-thin leading-8">
-                            <span className={"font-semibold"}>Software engineer</span> passionate about web development. <br/>
-                            Interested in working together? <span className={"font-semibold"}>Let&apos;s get in touch!</span>
+                            <span className={"font-semibold"}>Software engineer</span> passionate about web
+                            development. <br/>
+                            Interested in working together? <span
+                            className={"font-semibold"}>Let&apos;s get in touch!</span>
                             <br/>
-
                         </p>
                         <div className="flex justify-center space-x-4 mt-8">
-                            <button className="btn btn-primary">Contact me <FontAwesomeIcon icon={faChevronRight}/>
+                            <button className="btn btn-primary">
+                                Contact me <FontAwesomeIcon icon={faChevronRight}/>
                             </button>
-                            <button className="btn btn-secondary btn-outline">See my CV <FontAwesomeIcon
-                                icon={faChevronRight}/></button>
+                            <button className="btn btn-secondary btn-outline">
+                                See my CV <FontAwesomeIcon icon={faChevronRight}/>
+                            </button>
                         </div>
                         <div className="flex justify-center mt-8 space-x-2">
                             <a href="#" className="btn btn-ghost btn-circle">
@@ -36,9 +38,11 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="flex justify-center items-center">
-                        <img className="w-96 h-96 rounded-full"
-                             src="https://avatars.githubusercontent.com/u/92269810?v=4"
-                             alt="Marcel Horváth"/>
+                        <img
+                            className="w-96 h-96 rounded-full"
+                            src="https://avatars.githubusercontent.com/u/92269810?v=4"
+                            alt="Marcel Horváth"
+                        />
                     </div>
                 </div>
             </section>
--- portfolio/components/Grid.tsx
+++ portfolio/components/Grid.tsx
@@ -3,28 +3,39 @@ import {useEffect, useState} from 'react';
 
 export default function Grid() {
     const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+    const [isActive, setIsActive] = useState(false);
 
     const handleMouseMove = (event: MouseEvent) => {
         const {clientX, clientY} = event;
-        setMousePosition({x: clientX - (window.innerWidth / 2) - 128, y: clientY - (window.innerHeight / 2) - 128});
+        const scrollX = window.scrollX || window.pageXOffset;
+        const scrollY = window.scrollY || window.pageYOffset;
+        setMousePosition({x: clientX - 128 + scrollX, y: clientY - 180 + scrollY});
     };
 
     useEffect(() => {
-        window.addEventListener('mousemove', handleMouseMove);
+        if (isActive) {
+            window.addEventListener('mousemove', handleMouseMove);
+        } else {
+            window.removeEventListener('mousemove', handleMouseMove);
+        }
+
         return () => {
             window.removeEventListener('mousemove', handleMouseMove);
         };
-    }, []);
+    }, [isActive]);
 
     return (
-        <div className="fixed top-0 left-0 w-full h-full pointer-events-none">
+        <div
+            className="absolute top-0 left-0 w-full h-full"
+            onMouseEnter={() => setIsActive(true)}
+            onMouseLeave={() => setIsActive(false)}>
             <div
-                className="grid-light w-full h-full absolute top-0 left-0 pointer-events-auto"
+                className={`grid-light w-full h-full absolute top-0 left-0`}
                 style={{
-                    maskPosition: `${mousePosition.x + window.innerWidth / 2}px ${mousePosition.y + window.innerHeight / 2}px`,
-                }}
-            />
-            <div className="grid-dark w-full h-full absolute top-0 left-0 pointer-events-none"/>
+                    maskPosition: `${mousePosition.x}px ${mousePosition.y}px`,
+                    opacity: isActive ? 1 : 0,
+                }}/>
+            <div className="grid-dark w-full h-full absolute top-0 left-0"/>
         </div>
     );
 }
