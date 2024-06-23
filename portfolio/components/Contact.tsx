eaa29812b0ee733965d06d6ae0793cb153f7b2ab
Marcel_Horvath <marcel.horvath@tul.cz>
2024-06-23 21:44:54 +0200

    Contact

--- portfolio/components/Contact.tsx
+++ portfolio/components/Contact.tsx
@@ -1,59 +1,49 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faEnvelope, faBriefcase} from "@fortawesome/free-solid-svg-icons";
+
 const ContactForm = () => {
     return (
-        <section className="flex flex-col items-center justify-center w-full h-full py-8" id={"contact"}>
-            <h1 className="text-5xl font-bold">Kontaktujte nás</h1>
-            <div className="lg:flex justify-between w-full h-full py-10 space-y-10 lg:space-y-0">
-                <form className="form-control flex flex-col lg:w-1/2 mx-auto space-y-2">
-                    <label className="label">
-                        <span className="label-text">Name</span>
-                    </label>
+        <section className="flex flex-col items-center justify-center w-full h-full">
+            <h2 className="md:text-5xl text-4xl font-bold text-center uppercase mb-8 text-primary">Contact Me</h2>
+            <p className={"text-xl text-center mb-16"}>
+                I&apos;m here to help! If you have any questions, comments, or just want to say hi, feel free to reach
+                out.
+                I&apos;ll get back to you as soon as possible.
+            </p>
+
+            <div
+                className="flex flow-col md:flex-row justify-between space-x-32 md:space-x-32 w-full h-full space-y-10 md:space-y-0">
+
+                <form className="form-control flex flex-col md:w-1/2 mx-auto space-y-4">
+                    <h3 className={"text-2xl uppercase text-secondary font-bold"}>Contact form</h3>
                     <input type="text" placeholder="Name"
                            className="input input-bordered"/>
-
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
                     <input type="text" placeholder="Email"
                            className="input input-bordered"/>
-                    <label className="label">
-                        <span className="label-text">Message</span>
-                    </label>
                     <textarea placeholder="Message"
                               className="textarea textarea-bordered h-32"/>
                     <button type="submit" className="btn btn-primary">Send</button>
                 </form>
 
-                <div className="flex flex-col lg:w-1/2 mx-auto space-y-8  items-center">
-                    <div className="flex space-x-16 items-center my-4">
-                        <div className="avatar flex flex-col space-y-4 items-center justify-center">
-                            <div className="w-32 rounded-xl">
-                                <img src="https://avatars.githubusercontent.com/u/50490412?v=4" alt="avatar"
-                                     className="rounded-xl"/>
-                            </div>
-                            <span className="text-xl font-bold">Váš Jméno</span>
-                        </div>
-                        <div className="avatar flex flex-col space-y-4 items-center justify-center">
-                            <div className="w-32 rounded-xl">
-                                <img src="https://avatars.githubusercontent.com/u/50490412?v=4" alt="avatar"
-                                     className="rounded-xl"/>
-                            </div>
-                            <span className="text-xl font-bold">Váš Jméno</span>
+                <div className="flex flex-col md:w-1/2 mx-auto space-y-8">
+
+                    <div className={"text-lg flex flex-col"}>
+                        <h3 className={"text-2xl uppercase text-secondary font-bold mb-2"}>Contact info</h3>
+                        <div className={"flex flex-col space-y-2"}>
+                            <span className={""}><FontAwesomeIcon icon={faEnvelope}/> Email: mar.horv22@gmail.com</span>
+                            <span><FontAwesomeIcon icon={faBriefcase}/> ICO: 21234761</span>
                         </div>
                     </div>
-                    <div className="flex flex-col space-y-4 items-center justify-center">
-                        <div className="flex items-center space-x-4">
-                            <span className="link text-xl">LinkedIn: linkedin.com/in/vase-jmeno</span>
-                        </div>
-                        <div className="flex items-center space-x-4">
-                            <span className="link text-xl">GitHub: github.com/vase-jmeno</span>
-                        </div>
-                        <div className="flex items-center space-x-4">
-                            <span className="link text-xl">Telefon: +420 123 456 789</span>
-                        </div>
-                        <div className="flex items-center space-x-4">
-                            <a href="mailto:mar.horv22@gmail.com" className="link text-xl" target="_blank"
-                               rel="noopener noreferrer">
-                                Email: email@gmail.com
+
+                    <div className={"text-lg"}>
+                        <h3 className={"text-2xl uppercase text-secondary font-bold mb-2"}>Socials</h3>
+                        <div className={"flex flex-row space-x-2"}>
+                            <a href="#" className="btn btn-ghost btn-circle">
+                                <FontAwesomeIcon size="2x" icon={faGithub}/>
+                            </a>
+                            <a href="#" className="btn btn-ghost btn-circle">
+                                <FontAwesomeIcon size="2x" icon={faLinkedin}/>
                             </a>
                         </div>
                     </div>
