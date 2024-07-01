4669380d98f6790661c5be164fe88f4423002cfc
Marcel_Horvath <marcel.horvath@tul.cz>
2024-07-01 12:31:17 +0200

    production fix

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -4,19 +4,7 @@ const withNextIntl = createNextIntlPlugin("./locale/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    swcMinify: true,
-    output: "standalone",
-    webpack: (config, { dev }) => {
-        if (!dev) {
-            config.resolve.alias = {
-                ...config.resolve.alias,
-                'react-dom$': 'react-dom/profiling',
-                'scheduler/tracing': 'scheduler/tracing-profiling',
-            };
-        }
-        return config;
-    },
+    reactStrictMode: false, swcMinify: true, output: "standalone",
 };
 
 export default withNextIntl(nextConfig);
